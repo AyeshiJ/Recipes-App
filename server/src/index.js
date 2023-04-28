f19314d41e464549f4b7864ca72e64eafdb97e60
AyeshiJ <ayeshi.j12@gmail.com>
2023-04-28 16:28:15 +0530

    initial commit

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+import {userRouter} from './routes/users.js'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+
+mongoose.connect(
+    "mongodb+srv://ayeshi:ayeshiRecipePW21@recipes.gbyuivc.mongodb.net/recipes?retryWrites=true&w=majority",
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+);
+
+app.listen(3001, () => console.log("SERVER STARTED!"));
+
